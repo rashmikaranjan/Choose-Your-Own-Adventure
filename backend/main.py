d34d8d2576c758769c164a7d0a661c3f1a26ec00
Rashmika <rashmika.ranjan@outlook.com>
2025-09-18 20:23:25 -0400

    FastAPI setup

---
+++ backend/main.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI(
+    # set title and description
+    title="Choose Your Own Adventure Game API",
+    description="api to generate cool stories",
+    version="0.1.0",    # if we are in prod, we can update this everytime we make a change
+    docs_url="/docs",
+    redoc_url="/redoc",     # all the fastapi api's come with documentation which we can view from the website
+)
+
+# adding middleware
+# have our api be used from a different origin
+# origin = domain/ url the services is running on
+# frontend runs on localhost:x, but backend may run on localhost:y
+# since backend has builtin securities it won't allow it to communicate with anything that's not on the same port/ origin
+# by adding CORS, we enable certain origins/urls to interact with our backend
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],    # GET, POST, PUT
+    allow_headers=["*"],    # additional info
+)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app="main:app", host="0.0.0.0", port=8000, reload=True)
+        # uvicorn - webserver - allows to run the fastapi application - because it can' run unless it's connected to a server
+        
